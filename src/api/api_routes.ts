eaed6711b1640403815d3d6a1a437c68f1ee1968
jafet <mhjafeth@gmail.com>
2026-03-25 22:42:49 -0600

    /api agregado

--- src/api/api_routes.ts
+++ src/api/api_routes.ts
@@ -1,12 +1,12 @@
 
 export const apiRoutes = {
-    singIn: "api/auth/login",
-    singUp: "api/user/sing-up",
-    publicaciones: "api/publicaciones",
-    publicaciones_categorias: "api/publicaciones/categorias",
-    junta_directiva: "api/miembroDeJunta",
-    junta_directiva_inactivos: "api/miembroDeJunta/inactivos",
-    administrativo: "api/administrativo",
-    administrativo_inactivos: "api/administrativo/inactivos"
+    singIn: "/api/auth/login",
+    singUp: "/api/user/sing-up",
+    publicaciones: "/api/publicaciones",
+    publicaciones_categorias: "/api/publicaciones/categorias",
+    junta_directiva: "/api/miembroDeJunta",
+    junta_directiva_inactivos: "/api/miembroDeJunta/inactivos",
+    administrativo: "/api/administrativo",
+    administrativo_inactivos: "/api/administrativo/inactivos"
 }
 
